cdd6f5fed0ffc6394b488d01c98954ae51a6ab79
Jv2350 <tp8375735@gmail.com>
2025-01-22 13:14:44 +0530

    first commit

---
+++ lib/appwrite/index.ts
@@ -0,0 +1,56 @@
+"use server";
+
+import { Account, Avatars, Client, Databases, Storage } from "node-appwrite";
+import { appwriteConfig } from "@/lib/appwrite/config";
+import { cookies } from "next/headers";
+
+// Function to create a client for user sessions
+export const createSessionClient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId);
+
+  // Retrieve the appwrite-session cookie
+  const session = (await cookies()).get("appwrite-session");
+
+  // Handle missing session
+  if (!session || !session.value) {
+    console.error("Session cookie not found or invalid.");
+    throw new Error("No session. Redirect the user to /sign-in.");
+  }
+
+  // Set the session token in the Appwrite client
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+  };
+};
+
+// Function to create a client with admin privileges
+export const createAdminClient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId)
+    .setKey(appwriteConfig.secretKey); // Use the admin API key for authentication
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+    get avatars() {
+      return new Avatars(client);
+    },
+  };
+};
